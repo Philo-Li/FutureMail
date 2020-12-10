5628d0a30972a0fb04dbba6dc8fb667e8d8dede7
Philo <maerd.lll.p@gmail.com>
2020-12-10 12:24:22 +0800

    add useCreateLetter hooks

---
+++ src/hooks/useCreateLetter.js
@@ -0,0 +1,19 @@
+/* eslint-disable object-curly-newline */
+import { useMutation } from '@apollo/client';
+import { useHistory } from 'react-router-dom';
+
+import { CREATE_LETTER } from '../graphql/mutations';
+
+const useCreateArticle = () => {
+  const history = useHistory();
+  const [mutate, result] = useMutation(CREATE_LETTER);
+
+  const createArticle = async ({ title, text, sendTime }) => {
+    mutate({ variables: { title, text, sendTime } });
+    history.push('/');
+  };
+
+  return [createArticle, result];
+};
+
+export default useCreateArticle;
